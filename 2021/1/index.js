9c3126be22c8d6021be1040c9462f04b96c134f1
Josef Budniakiewicz <josefbud@gmail.com>
2021-12-01 09:41:47 -0500

    2021: Day 1, Part 2

--- 2021/1/index.js
+++ 2021/1/index.js
@@ -1,16 +1,52 @@
 const INPUT = require('./input');
 
-let increases = 0;
+const partOne = () => {
+  let increases = 0;
 
-for (firstIndex in INPUT) {
-  if (firstIndex === 0) continue;
+  for (index in INPUT) {
+    if (index === 0) continue;
 
-  const num = INPUT[firstIndex];
-  const previousNum = INPUT[firstIndex - 1];
+    const num = INPUT[index];
+    const previousNum = INPUT[index - 1];
 
-  if (num > previousNum) {
-    increases += 1;
+    if (num > previousNum) {
+      increases += 1;
+    }
   }
-}
 
-console.log(increases);
+  console.log(increases);
+};
+
+// partOne();
+
+const partTwo = () => {
+  let increases = 0;
+  let firstSum;
+  let secondSum;
+
+  for (index in INPUT) {
+    if (index === 0 || index === 1) continue;
+
+    const firstNum = INPUT[index - 2];
+    const secondNum = INPUT[index - 1];
+    const thirdNum = INPUT[index];
+    const sum = firstNum + secondNum + thirdNum;
+
+    if (!firstSum) {
+      firstSum = sum;
+      continue;
+    }
+
+    secondSum = sum;
+
+    if (secondSum > firstSum) {
+      increases += 1;
+    }
+
+    firstSum = sum;
+  }
+
+  console.log(increases);
+};
+
+partTwo();
