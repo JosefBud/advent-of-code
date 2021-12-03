e93e4fdfbecad9b004d9452e74179e94d60e9f48
Josef Budniakiewicz <josefbud@gmail.com>
2021-12-03 17:39:11 -0500

    2021: Day 3, Part 2

--- 2021/3/index.js
+++ 2021/3/index.js
@@ -44,4 +44,110 @@ const partOne = () => {
   console.log(gammaDecimal * epsilonDecimal);
 };
 
-partOne();
+// partOne();
+
+const partTwo = () => {
+  const bitCountObject = {
+    0: 0,
+    1: 0,
+    indecesWith0: [],
+    indecesWith1: []
+  };
+  // 12 bit counts
+  const bitCounts = [
+    { ...bitCountObject },
+    { ...bitCountObject },
+    { ...bitCountObject },
+    { ...bitCountObject },
+    { ...bitCountObject },
+    { ...bitCountObject },
+    { ...bitCountObject },
+    { ...bitCountObject },
+    { ...bitCountObject },
+    { ...bitCountObject },
+    { ...bitCountObject },
+    { ...bitCountObject }
+  ];
+
+  let oxygenRating;
+  let scrubberRating;
+
+  let updatingInput = [...INPUT];
+
+  for (let i = 0; i < 12; i++) {
+    bitCounts[i] = { ...bitCountObject };
+
+    for (inputIndex in updatingInput) {
+      const binaryNum = updatingInput[inputIndex];
+      const bit = Number(binaryNum.split('')[i]);
+      bitCounts[i][bit] += 1;
+
+      if (bit === 0) {
+        const currentArray = bitCounts[i].indecesWith0;
+        bitCounts[i].indecesWith0 = [...currentArray, inputIndex];
+      } else {
+        const currentArray = bitCounts[i].indecesWith1;
+        bitCounts[i].indecesWith1 = [...currentArray, inputIndex];
+      }
+    }
+
+    const bitCountFinal = bitCounts[i];
+
+    const indecesToContinue =
+      bitCountFinal[0] > bitCountFinal[1]
+        ? bitCountFinal.indecesWith0
+        : bitCountFinal.indecesWith1;
+
+    updatingInput = updatingInput.filter((ele, index) =>
+      indecesToContinue.includes(index.toString())
+    );
+
+    if (updatingInput.length === 1) {
+      break;
+    }
+  }
+
+  oxygenRating = parseInt(updatingInput[0], 2);
+  updatingInput = [...INPUT];
+
+  // so much repeated code that hurts my heart but its friday and I want to leave
+
+  for (let i = 0; i < 12; i++) {
+    bitCounts[i] = { ...bitCountObject };
+
+    for (inputIndex in updatingInput) {
+      const binaryNum = updatingInput[inputIndex];
+      const bit = Number(binaryNum.split('')[i]);
+      bitCounts[i][bit] += 1;
+
+      if (bit === 0) {
+        const currentArray = bitCounts[i].indecesWith0;
+        bitCounts[i].indecesWith0 = [...currentArray, inputIndex];
+      } else {
+        const currentArray = bitCounts[i].indecesWith1;
+        bitCounts[i].indecesWith1 = [...currentArray, inputIndex];
+      }
+    }
+
+    const bitCountFinal = bitCounts[i];
+
+    const indecesToContinue =
+      bitCountFinal[1] < bitCountFinal[0]
+        ? bitCountFinal.indecesWith1
+        : bitCountFinal.indecesWith0;
+
+    updatingInput = updatingInput.filter((ele, index) =>
+      indecesToContinue.includes(index.toString())
+    );
+
+    if (updatingInput.length === 1) {
+      break;
+    }
+  }
+
+  scrubberRating = parseInt(updatingInput[0], 2);
+
+  console.log(oxygenRating * scrubberRating);
+};
+
+partTwo();
