b2434a6be2f8e4e0b72b262d8c3e355dc9f6192a
Josef Budniakiewicz <josefbud@gmail.com>
2023-12-04 22:22:06 -0500

    Add util function + tests ahead of day 3 puzzle

---
+++ util.spec.ts
@@ -0,0 +1,89 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import { FoundNumber, findNumbersInString } from './util';
+
+describe('findNumbersInString', () => {
+  it('Works as expected with numbers neither at beginning nor end', () => {
+    const TEST_STRING = 'are4893burp944dingle';
+    const EXPECTED: FoundNumber[] = [
+      { digitLength: 4, index: 3, number: 4893 },
+      { digitLength: 3, index: 11, number: 944 },
+    ];
+    const foundNumbers = findNumbersInString(TEST_STRING);
+    assert.strictEqual(foundNumbers.length, 2);
+    foundNumbers.forEach((foundNumber, idx) => {
+      assert.deepStrictEqual(foundNumber, EXPECTED[idx]);
+    });
+  });
+
+  it('Works as expected with number at beginning', () => {
+    const TEST_STRING = '994421spurgle8dingle';
+    const EXPECTED: FoundNumber[] = [
+      { digitLength: 6, index: 0, number: 994421 },
+      { digitLength: 1, index: 13, number: 8 },
+    ];
+    const foundNumbers = findNumbersInString(TEST_STRING);
+    assert.strictEqual(foundNumbers.length, 2);
+    foundNumbers.forEach((foundNumber, idx) => {
+      assert.deepStrictEqual(foundNumber, EXPECTED[idx]);
+    });
+  });
+
+  it('Works as expected with number at the end', () => {
+    const TEST_STRING = 'biffit4854stinker123jeebus46';
+    const EXPECTED: FoundNumber[] = [
+      { digitLength: 4, index: 6, number: 4854 },
+      { digitLength: 3, index: 17, number: 123 },
+      { digitLength: 2, index: 26, number: 46 },
+    ];
+    const foundNumbers = findNumbersInString(TEST_STRING);
+    assert.strictEqual(foundNumbers.length, 3);
+    foundNumbers.forEach((foundNumber, idx) => {
+      assert.deepStrictEqual(foundNumber, EXPECTED[idx]);
+    });
+  });
+
+  it('Works as expected with number at beginning and end', () => {
+    const TEST_STRING = '123luna844';
+    const EXPECTED: FoundNumber[] = [
+      { digitLength: 3, index: 0, number: 123 },
+      { digitLength: 3, index: 7, number: 844 },
+    ];
+    const foundNumbers = findNumbersInString(TEST_STRING);
+    assert.strictEqual(foundNumbers.length, 2);
+    foundNumbers.forEach((foundNumber, idx) => {
+      assert.deepStrictEqual(foundNumber, EXPECTED[idx]);
+    });
+  });
+
+  it('Works as expected with leading-zero numbers', () => {
+    const TEST_STRING = '0012maker044welp';
+    const EXPECTED: FoundNumber[] = [
+      { digitLength: 2, index: 0, number: 12 },
+      { digitLength: 2, index: 9, number: 44 },
+    ];
+    const foundNumbers = findNumbersInString(TEST_STRING);
+    assert.strictEqual(foundNumbers.length, 2);
+    foundNumbers.forEach((foundNumber, idx) => {
+      assert.deepStrictEqual(foundNumber, EXPECTED[idx]);
+    });
+  });
+
+  it('Works as expected with no numbers', () => {
+    const TEST_STRING = 'luna.is.a.stinker';
+    const foundNumbers = findNumbersInString(TEST_STRING);
+    assert.strictEqual(foundNumbers.length, 0);
+  });
+
+  it('Works as expected with only number', () => {
+    const TEST_STRING = '87654';
+    const EXPECTED: FoundNumber[] = [
+      { digitLength: 5, index: 0, number: 87654 },
+    ];
+    const foundNumbers = findNumbersInString(TEST_STRING);
+    assert.strictEqual(foundNumbers.length, 1);
+    foundNumbers.forEach((foundNumber, idx) => {
+      assert.deepStrictEqual(foundNumber, EXPECTED[idx]);
+    });
+  });
+});
---
+++ util.ts
@@ -0,0 +1,41 @@
+export type FoundNumber = {
+  index: number;
+  number: number;
+  digitLength: number;
+};
+type TFindNumbersInString = (input: string) => FoundNumber[];
+export const findNumbersInString: TFindNumbersInString = (input) => {
+  const inputSplit = input.split('');
+  const returns: FoundNumber[] = [];
+  for (let i = 0; i < inputSplit.length - 1; i++) {
+    const char = inputSplit[i];
+    const number = {
+      index: -1,
+      number: 0,
+      digitLength: 0,
+    };
+    let totalNumber = '';
+    if (isNaN(parseInt(char))) {
+      continue;
+    }
+    totalNumber += char;
+    number.index = i;
+    for (let a = i + 1; a < inputSplit.length; a++) {
+      const nextChar = inputSplit[a];
+      if (isNaN(parseInt(nextChar))) {
+        i = a;
+        break;
+      }
+      totalNumber += nextChar;
+      if (a === inputSplit.length - 1) {
+        i = inputSplit.length + 1;
+        break;
+      }
+    }
+    number.number = parseInt(totalNumber);
+    number.digitLength = parseInt(totalNumber).toString().length;
+    returns.push(number);
+  }
+
+  return returns;
+};
