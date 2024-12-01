0d610000ea9d91d7f11975ecee2bc351496ef854
Josef Budniakiewicz <josefbud@gmail.com>
2024-12-01 10:24:13 -0500

    2024: Day 1

---
+++ 2024/1/part1.ts
@@ -0,0 +1,21 @@
+
+import { input } from './input';
+
+const firstList: number[] = [];
+const secondList: number[] = [];
+const list = input.split('\n').forEach((value) => {
+  const nums = value.split('   ').map(Number);
+  firstList.push(nums[0]);
+  secondList.push(nums[1]);
+})
+firstList.sort();
+secondList.sort();
+
+const difference: number[] = firstList.map((first, idx) => {
+  const second = secondList[idx];
+  return Math.abs(second - first);
+});
+
+const total = difference.reduce((a, b) => a + b);
+
+console.log(total);
---
+++ 2024/1/part2.ts
@@ -0,0 +1,19 @@
+
+import { input } from './input';
+// import { input } from './test-input';
+
+const firstList: number[] = [];
+const secondList: number[] = [];
+const list = input.split('\n').forEach((value) => {
+  const nums = value.split('   ').map(Number);
+  firstList.push(nums[0]);
+  secondList.push(nums[1]);
+})
+firstList.sort();
+secondList.sort();
+
+const similarityScores: number[] = firstList.map((first) => {
+  return first * secondList.filter(n => n === first).length;
+});
+
+console.log(similarityScores.reduce((a, b) => a + b))
