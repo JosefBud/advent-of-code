35b59944cbe4991e260b12536d2ff7c6d9400902
Josef Budniakiewicz <josefbud@gmail.com>
2024-12-04 00:18:52 -0500

    Replace `downloadInput` script with a newer, better script that generates all of the necessary directories and files. Add `npm start` script. Add util functions to read test inputs from text files.

---
+++ scripts/daily-bootstrap.ts
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+import fs from 'node:fs/promises';
+import fsSync from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import spawn from 'node:child_process';
+import env from '../.env.json' with { type: 'json' };
+
+// Idk, this just looks cleaner than a string with inconsistent indenting. Sue me.
+const SOLUTION_TEMPLATE = [
+  "import { getExampleInput, getInput } from '../../util.ts';",
+  '',
+  'const input = getExampleInput();',
+  '// const input = getInput();',
+  '',
+].join('\n');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const sleep = async (ms: number = 1000) =>
+  new Promise((r) => setTimeout(r, ms));
+
+/**
+ * Bootstraps the directory and files for the day's puzzle.
+ *
+ * Always checks for existence of files before attempting to write, so that this can be
+ * run repeatedly if one part of it fails.
+ */
+async function main() {
+  const year = new Date().getFullYear();
+  const day = new Date().getDate();
+  const dirPath = __dirname + `/../${year}/${day}`;
+  const puzzleUrl = `https://adventofcode.com/${year}/day/${day}`;
+
+  // Create directory for today
+  if (!fsSync.existsSync(dirPath)) {
+    await fs.mkdir(dirPath);
+    console.log("Created today's directory");
+  }
+
+  // Create part1 + part2
+  if (!fsSync.existsSync(`${dirPath}/part1.ts`)) {
+    await fs.writeFile(`${dirPath}/part1.ts`, SOLUTION_TEMPLATE);
+    console.log('Created part1.ts');
+  }
+  if (!fsSync.existsSync(`${dirPath}/part2.ts`)) {
+    await fs.writeFile(`${dirPath}/part2.ts`, SOLUTION_TEMPLATE);
+    console.log('Created part2.ts');
+  }
+
+  // Fetch the example input and write it to text-input.txt
+  if (!fsSync.existsSync(`${dirPath}/example-input.txt`)) {
+    console.log('Fetching example input for day', day);
+    const res = await fetch(puzzleUrl);
+    const html = await res.text();
+    const matches = html.match(/example[\s\S]*?<code>([\s\S]*?)<\/code>/);
+    if (matches?.[1]) {
+      await fs.writeFile(`${dirPath}/example-input.txt`, matches[1]);
+      console.log('Created text-input.txt');
+    } else {
+      console.error('There are no matches for day', day);
+      await fs.writeFile(`${dirPath}/example-page.txt`, html);
+    }
+  }
+
+  if (!fsSync.existsSync(`${dirPath}/input.txt`)) {
+    console.log('Fetching input for day', day);
+    const res = await fetch(`${puzzleUrl}/input`, {
+      headers: { Accept: 'text/plain', cookie: env.cookie },
+    });
+    if (!res.ok) {
+      console.error('Getting input failed, most likely due to a bad cookie');
+      return;
+    }
+    const text = await res.text();
+    await fs.writeFile(`${dirPath}/input.txt`, text);
+    console.log('Created input.txt');
+  }
+
+  spawn.exec(`open ${puzzleUrl}`);
+}
+
+main();
--- util.ts
+++ util.ts
@@ -1,3 +1,9 @@
+import fsSync from 'node:fs';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+export const __filename = () => fileURLToPath(import.meta.url);
+export const __dirname = () => dirname(__filename());
 export const int = (input: string): number => parseInt(input, 10);
 
 export type FoundNumber = {
@@ -40,4 +46,39 @@ export const findNumbersInString: TFindNumbersInString = (input) => {
   }
 
   return returns;
+};
+
+/**
+ * Gets the file path for a function calling from any {year}/{day} directory
+ */
+export const getCallerFilePath = () => {
+  const callStack = new Error().stack;
+  if (callStack) {
+    // Find the part of the call stack that has {year}/{day} in the path. This will break after 2099. Good luck with that.
+    const [stack] = callStack
+      .split('\n')
+      .filter((stack) => stack.match(/\/20[2-9][0-9]\/[1-9][1-9]*\//));
+
+    // Then capture the file path out of the full error stack string
+    const [, path] = stack.match(/file:\/\/[\s\S]*?([\s\S]*?):[0-9]/) ?? [];
+    if (path) {
+      return dirname(path);
+    }
+  }
+
+  throw new Error(
+    'Unable to resolve call stack or input path while getting input',
+  );
+};
+
+export const getInput = () => {
+  const path = getCallerFilePath();
+  return fsSync.readFileSync(path + '/input.txt', { encoding: 'ascii' });
+};
+
+export const getExampleInput = () => {
+  const path = getCallerFilePath();
+  return fsSync.readFileSync(path + '/example-input.txt', {
+    encoding: 'ascii',
+  });
 };
